1d39210913249c51252645100a70f2dc4451fae6
Bondar Semeon <sema.bond.2003@mail.ru>
2023-12-24 20:44:20 +0300

    Lab_13 Done 2.0

--- labs/Lab_13_python_oop/solution/main.py
+++ labs/Lab_13_python_oop/solution/main.py
@@ -1,28 +1,14 @@
-import os
+import xlsxwriter
 import json
 from datetime import datetime
 from writer import XlsAnalyticPaymentWriter
 
-def load_data(file_path):
-    full_path = os.path.join(
-        "C:\\Users\\Semyon\\Desktop\\gg\\labs\\Lab_13_python_oop",
-        file_path
-    )
-
-    with open(full_path, 'r', encoding='utf-8') as file:
-        data = json.load(file)
-    return data
-
 if __name__ == '__main__':
-    data_clients = load_data('clients.json')
-    data_payments = load_data('payments.json')
-
+    with open('clients.json', 'r', encoding='utf-8') as f:
+        data_clients = json.load(f)
+    with open('payments.json', 'r', encoding='utf-8') as f:
+        data_payments = json.load(f)
     data = {'clients': data_clients['clients'], 'payments': data_payments['payments']}
 
-    timestamp = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
-    output_file = f'Lab_13_VVS{timestamp}.xlsx'
-
-    xls_writer = XlsAnalyticPaymentWriter(data)
-    xls_writer.write_excel_report(output_file)
-
-    print(f"Отчет успешно создан. Итоговый файл: {output_file}")
+    output = XlsAnalyticPaymentWriter(data)
+    output.write_excel_report(f'payments_analytics_{datetime.now().strftime('%Y-%m-%d')}.xlsx')
--- labs/Lab_13_python_oop/solution/writer.py
+++ labs/Lab_13_python_oop/solution/writer.py
@@ -1,28 +1,34 @@
 import xlsxwriter
 from datetime import datetime
 from base import BaseXlsBlock
-from blocks import TopPayersBlock, TopCitiesBlock, AccountStatusBlock
+from block import HeaderBlock, QuartetPaymentBlock, CustomerGeographyBlock, BankAccountBlock
 
 class XlsAnalyticPaymentWriter:
     ANALYTICS_BLOCKS_CLASSES = [
-        TopPayersBlock,
-        TopCitiesBlock,
-        AccountStatusBlock ]
+        HeaderBlock,
+        QuartetPaymentBlock,
+        CustomerGeographyBlock,
+        BankAccountBlock,
+    ]
 
     def __init__(self, data):
         self.data = data
 
     def write_excel_report(self, output_file):
         workbook = xlsxwriter.Workbook(output_file)
-        worksheet = workbook.add_worksheet()
+        worksheet = workbook.add_worksheet('analytics')
+        worksheet.set_column(0, 0, 40)
+        worksheet.set_column(1, 5, 40)
 
         row = 0
         col = 0
 
         for block_class in self.ANALYTICS_BLOCKS_CLASSES:
-            block_instance = block_class(worksheet, row, col, self.data)
-            block_instance.write_header()
-            block_instance.write_data()
-            row += 12
+            block_instance = block_class(worksheet, workbook, row, col, self.data)
+            block_instance.writeHeaderCol()
+            block_instance.writeData()
+            if row == 0:
+                row -= 9
+            row += 14
 
         workbook.close()
