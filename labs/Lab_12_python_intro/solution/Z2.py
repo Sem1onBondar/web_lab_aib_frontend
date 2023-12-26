2beb45c6e6f3ea41cf9d02bb4d8b42d966bf809b
Bondar Semeon <sema.bond.2003@mail.ru>
2023-12-27 02:14:57 +0300

    Lab_12 Done 2.0

--- labs/Lab_12_python_intro/solution/Z2.py
+++ labs/Lab_12_python_intro/solution/Z2.py
@@ -1,10 +1,19 @@
-def sum_median(arr):
-    count = 0
-    for item in arr:
-        tarr = sorted(arr[:item])
-        count += tarr[len(tarr) // 2] if len(tarr) % 2 != 0 else tarr[len(tarr) // 2 - 1]
-    return count
+def calc_sum_medians(sum_medians, sub_seq_len, numbers):
+    temp_seq = numbers[:sub_seq_len]
+    temp_seq.sort()
+    if len(temp_seq) % 2 == 1:
+        if len(temp_seq) == 1:
+            sum_medians += temp_seq[0]
+        else:
+            sum_medians += temp_seq[((len(temp_seq) + 1) // 2) - 1]
+    elif len(temp_seq) % 2 == 0:
+        sum_medians += temp_seq[(len(temp_seq) // 2) - 1]
+    sub_seq_len += 1
+    if sub_seq_len > num_count:
+        return sum_medians
+    else:
+        return calc_sum_medians(sum_medians, sub_seq_len, numbers)
 
-
-print(sum_median([5, 10, 8, 1, 7, 3, 9, 6, 2, 4]))
-# print(sum_median([5, 3, 1, 2, 4]))
+num_count = int(input())
+numbers = list(map(int, input().split()))
+print(calc_sum_medians(0, 1, numbers))
