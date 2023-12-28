5bb96ae8dbbf25719f2ba872750d99c5bcabc6f2
Bondar Semeon <sema.bond.2003@mail.ru>
2023-12-28 12:34:38 +0300

    Lab_12 Done Z_2 vrode normik

--- labs/Lab_12_python_intro/solution/Z2.py
+++ labs/Lab_12_python_intro/solution/Z2.py
@@ -1,19 +1,15 @@
-def calc_sum_medians(sum_medians, sub_seq_len, numbers):
-    temp_seq = numbers[:sub_seq_len]
-    temp_seq.sort()
-    if len(temp_seq) % 2 == 1:
-        if len(temp_seq) == 1:
-            sum_medians += temp_seq[0]
-        else:
-            sum_medians += temp_seq[((len(temp_seq) + 1) // 2) - 1]
-    elif len(temp_seq) % 2 == 0:
-        sum_medians += temp_seq[(len(temp_seq) // 2) - 1]
-    sub_seq_len += 1
-    if sub_seq_len > num_count:
-        return sum_medians
-    else:
-        return calc_sum_medians(sum_medians, sub_seq_len, numbers)
+import heapq
 
-num_count = int(input())
-numbers = list(map(int, input().split()))
-print(calc_sum_medians(0, 1, numbers))
+def find(n, arr):
+    max, min, result = [], [], 0
+    for i in range(n):
+        heapq.heappush(max, -arr[i])
+        heapq.heappush(min, -heapq.heappop(max))
+
+        if len(min) > len(max):
+            heapq.heappush(max, -heapq.heappop(min))
+        result -= max[0]
+    return result
+
+n, arr = int(input()), list(map(int, input().split()))
+print(find(n, arr))
